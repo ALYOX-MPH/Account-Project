0e161e4deafeb1c7b68215cd31b750cd28929064
FranciscoGXX <gabrielxx188@gmail.com>
2025-10-21 07:42:16 -0400

    añadiendo elementos basicos para el formulario productServices

--- src/pages/product_services/components/ProductForm.tsx
+++ src/pages/product_services/components/ProductForm.tsx
@@ -10,10 +10,27 @@ export function ProductForm() {
                 <div className="flex flex-col mb-4 w-full mr-2 text-white ">
                     <label htmlFor="Name">Nombre</label>
                     <input 
-                    type="text" placeholder="Juan Antonio" name="Name" id="Name" 
+                    type="text" 
+                    placeholder="Juan Antonio" 
+                    name="Name" 
+                    id="Name"
+                    
+                    />
+                </div>
+
+                {/* Unit field */}
+                <div className="flex flex-col mb-4 w-full mr-2 text-white ">
+                    <label htmlFor="">Unidad de medida</label>
+
+                   <select name="Metrics" id="">
+                        <option  value="1">Unidad</option>
+                        <option value="2">Paquete</option>
+                        <option value="3">Botella</option>
+                        <option value="4">Docena</option>
+                   </select>
+
                     
                     
-                />
                 </div>
             </div>
         </div>
